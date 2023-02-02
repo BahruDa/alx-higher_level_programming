06a53897364829d0f9c998e816e1a23a3794e216
BahruDa <bahrudari68@gmail.com>
2023-02-02 15:27:40 +0300

    .

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,43 +1,10 @@
 #!/usr/bin/python3
-"""
-<<<<<<< HEAD
-    Insert here module comment
-    Write a function that prints My name is <first name> <last name>
-    Prototype: def say_my_name(first_name, last_name=""):
-    first_name and last_name must be strings otherwise,
-    raise a TypeError exception with the message
-    first_name must be a string or last_name must be a string
-    You are not allowed to import any module
-"""
+say_my_name = __import__('3-say_my_name').say_my_name
 
-
-def say_my_name(first_name, last_name=""):
-    """ print my first and last name """
-    str_error = "first_name must be a string or last_name must be a string"
-    if type(first_name) is not str or type(last_name) is not str:
-        raise TypeError(str_error)
-    print("My name is", first_name, last_name)
-
-=======
-Print string
-"""
-def say_my_name(first_name, last_name=""):
-   """Concatenate both parameters and print
-   Parameters:
-   first_name = first name
-   last_name = last name
-   Local Variables:
-   fullname = empty string
-   Return: None
-   """
-    fullname = ""
-    if first_name and type(first_name) != str:
-        raise TypeError('first_name must be a string')
-    elif first_name and type(first_name) == str:
-        fullname += first_name + " "
-    if last_name and type(last_name) != str:
-        raise TypeError('last_name must be a string')
-    elif last_name and type(last_name) == str:
-        fullname += last_name
-    print("My name is {:s}".format(fullname))
->>>>>>> 67c6bd079ffa0ebfb48ef2ae9bcd10f2d4c266a9
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
