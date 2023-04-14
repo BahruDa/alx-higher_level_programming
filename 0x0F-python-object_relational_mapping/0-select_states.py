66c78c1a240d2889bd50b1fd238a408c8f56668a
BahruDa <bahrudari68@gmail.com>
2023-04-14 07:07:43 +0300

    .

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db_connection = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                                    db=argv[3])
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    db_connection.close()
